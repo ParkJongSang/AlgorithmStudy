d0ca2b6f9a000ebe148f03ed05834dac33f25132
Sokiwar <whdtkd546@gmail.com>
2020-03-15 16:12:55 +0900

    [UPDATE] vat -> let

--- swift/leet_code_1108.swift
+++ swift/leet_code_1108.swift
@@ -1,6 +1,6 @@
 class Solution {
     func defangIPaddr(_ address: String) -> String {
-        var splitedAddress = address.split(separator: ".")
+        let splitedAddress = address.split(separator: ".")
         return splitedAddress.joined(separator: "[.]")
     }
 }
